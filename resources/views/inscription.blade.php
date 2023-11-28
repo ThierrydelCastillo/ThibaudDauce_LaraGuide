d354dc199eac3499f16b0500ac67e76741bfe281
ThierrydelCastillo <dev@del-castillo.fr>
2023-11-28 07:45:27 +0100

    17 - Styliser notre application avec Bulma

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -1,24 +1,43 @@
 @extends('layout')
 
 @section('contenu')
-    <form action="/inscription" method="post">
+    <form action="/inscription" method="post" class="section">
         {{ csrf_field() }}
-        
-        <p><input type="email" name="email" placeholder="Email" value="{{ old('email')}}"></p>
-        @if ($errors->has('email'))
-            <p>{{ $errors->first('email') }}</p>
-        @endif
-        
-        <p><input type="password" name="password" placeholder="Mot de passe"></p>
-        @if ($errors->has('password'))
-            <p>{{ $errors->first('password') }}</p>
-        @endif
 
-        <p><input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)"></p>
-        @if ($errors->has('password_confirmation'))
-            <p>{{ $errors->first('password_confirmation') }}</p>
-        @endif
+        <div class="field">
+            <label class="label">Adresse email</label>
+            <div class="control">
+                <input class="input" type="email" name="email" value="{{ old('email')}}">
+            </div>
+            @if ($errors->has('email'))
+                <p class="help is-danger">{{ $errors->first('email') }}</p>
+            @endif
+        </div>
+
+        <div class="field">
+            <label class="label">Mot de passe</label>
+            <div class="control">
+                <input class="input" type="password" name="password">
+            </div>
+            @if ($errors->has('password'))
+                <p class="help is-danger">{{ $errors->first('password') }}</p>
+            @endif
+        </div>
         
-        <p><input type="submit" value="M'inscrire"></p>
+        <div class="field">
+            <label class="label">Mot de passe (confirmation)</label>
+            <div class="control">
+                <input class="input" type="password" name="password_confirmation">
+            </div>
+            @if ($errors->has('password_confirmation'))
+                <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+            @endif
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button class="button is-link" type="submit">M'inscrire</button>
+            </div>
+        </div>
     </form>
 @endsection
