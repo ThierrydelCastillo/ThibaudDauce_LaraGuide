f0c5df13d70b51ca22d40b8cab6894710c799147
ThierrydelCastillo <dev@del-castillo.fr>
2023-11-28 06:54:36 +0100

    13 - Astuces avec Eloquent et MassAssignmentException

--- app/Utilisateur.php
+++ app/Utilisateur.php
@@ -5,5 +5,5 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Utilisateur extends Model {
-
+    protected $fillable = ['email', 'mot_de_passe'];
 }
--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,10 @@ Route::get('/inscription', function () {
 });
 
 Route::post('/inscription', function () {
-    $utilisateur = new App\Utilisateur();
-    $utilisateur->email = request('email');
-    $utilisateur->mot_de_passe = bcrypt(request('password'));
-
-    $utilisateur->save(); // INSERT en base de données
+    $utilisateur = App\Utilisateur::create([
+        'email' => request('email'),
+        'mot_de_passe' => bcrypt(request('password')),
+    ]);
 
     return 'Nous avons reçu votre email qui est ' . request('email') . ' et votre mot de passe est ' . request('password');
 });
