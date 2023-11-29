251356628393447b9baf1fab14c6f2bdb1c797b3
ThierrydelCastillo <dev@del-castillo.fr>
2023-11-29 09:14:24 +0100

    26 - Mettre à jour nos utilisateurs avec Laravel Eloquent

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -22,4 +22,28 @@ class CompteController extends Controller
 
         return redirect('/');
     }
+
+    public function modificationMotDePasse()
+    {
+        if (auth()->guest()) {
+            flash('Vous devez être connecté pour voir cette page.')->error();
+            return redirect('/connexion');
+        }
+
+        request()->validate([
+            'password' => ['required', 'confirmed', 'min:8'],
+            'password_confirmation' => ['required'],
+        ]);
+
+        // $utilisateur = auth()->user();
+        // $utilisateur->mot_de_passe = bcrypt(request('password'));
+        // $utilisateur->save();
+        auth()->user()->update([
+            'mot_de_passe' => bcrypt(request('password')),
+        ]);
+
+        flash("Votre mot de passe à bien été mis à jour.")->success();
+
+        return redirect('/mon-compte');
+    }
 }
--- resources/views/mon-compte.blade.php
+++ resources/views/mon-compte.blade.php
@@ -8,4 +8,34 @@
 
         <a href="/deconnexion" class="button">Déconnexion</a>
     </div>
+
+    <form action="/modification-mot-de-passe" method="post" class="section">
+        {{ csrf_field() }}
+
+        <div class="field">
+            <label class="label">Nouveau mot de passe</label>
+            <div class="control">
+                <input class="input" type="password" name="password">
+            </div>
+            @if ($errors->has('password'))
+                <p class="help is-danger">{{ $errors->first('password') }}</p>
+            @endif
+        </div>
+        
+        <div class="field">
+            <label class="label">Mot de passe (confirmation)</label>
+            <div class="control">
+                <input class="input" type="password" name="password_confirmation">
+            </div>
+            @if ($errors->has('password_confirmation'))
+                <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+            @endif
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button class="button is-link" type="submit">Modifier mon mot de passe</button>
+            </div>
+        </div>
+    </form>
 @endsection
