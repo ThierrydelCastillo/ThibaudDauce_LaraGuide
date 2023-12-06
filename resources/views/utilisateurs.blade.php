31e66ac038add066f888965ff54bcc820b89bbb0
ThierrydelCastillo <dev@del-castillo.fr>
2023-12-06 13:35:50 +0100

    38 - Afficher une relation Many to Many

--- resources/views/utilisateurs.blade.php
+++ resources/views/utilisateurs.blade.php
@@ -1,13 +1,36 @@
 @extends('layout')
 
 @section('contenu')
-    <h1 class="title is-l">Les Utilisateurs</h1>
+    <div class="section">
+        <h1 class="title is-l">Bienvenue !</h1>
 
-    <ul>
-        @foreach ($utilisateurs as $utilisateur)
-            <li>
-                <a href="/{{ $utilisateur->email }}">{{ $utilisateur->email }}</a>
-            </li>
-        @endforeach
-    </ul>
+        @auth
+            <div class="section">
+                
+                <h2>Les utilisateurs suivis</h2>
+                <ul>
+                    @forelse(auth()->user()->suivis as $utilisateur)
+                        <li>
+                            <a href="/{{ $utilisateur->email }}">{{ $utilisateur->email }}</a>
+                        </li>
+                    @empty
+                        Vous ne suivez aucun utilisateur actuellement
+                    @endforelse
+                </ul>
+            </div>
+        @endauth
+
+        <div class="section">
+            <h2>Tous les utilisateurs</h2>
+        
+            <ul>
+                @foreach ($utilisateurs as $utilisateur)
+                    <li>
+                        <a href="/{{ $utilisateur->email }}">{{ $utilisateur->email }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    
+    </div>
 @endsection
